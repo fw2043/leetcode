2bff89df2deb35263356f351d26a292e6b7f60f4
xliu <xialiu@quickenloans.com>
2021-10-20 16:19:17 -0400

    Create 350-Intersection of Two Arrays II.py

---
+++ Binary Search/350-Intersection of Two Arrays II.py
@@ -0,0 +1,18 @@
+"""
+Given two integer arrays nums1 and nums2, return an array of their intersection.
+Each element in the result must appear as many times as it shows in both arrays
+and you may return the result in any order.
+"""
+##########################################################################
+# Follow up:
+########################################################################
+"""
+What if the given array is already sorted? How would you optimize your algorithm? --- Binary search
+What if nums1's size is small compared to nums2's size? Which algorithm is better? --- call the function itself to search each element  from smaller list
+What if elements of nums2 are stored on disk, and the memory is limited 
+such that you cannot load all elements into the memory at once?
+"""
+####################################################################################################
+"""
+Method similar to leetcode 349
+"""
