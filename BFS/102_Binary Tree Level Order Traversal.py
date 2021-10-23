a2c341d6943137b707777cfabc734bf9298ef4fa
xliu <xialiu@quickenloans.com>
2021-10-22 23:49:31 -0400

    two methods

--- BFS/102_Binary Tree Level Order Traversal.py
+++ BFS/102_Binary Tree Level Order Traversal.py
@@ -49,3 +49,38 @@ class Solution:
                 res.append(level)
 
         return res
+
+""""
+Method2:
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        current_level = [root]
+        results = []
+        while current_level:
+            res = []
+            next_level = []
+            for node in current_level:
+                res.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            current_level = next_level
+            results.append(res)
+
+        return results
+
+
+
