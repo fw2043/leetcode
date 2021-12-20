a2b0eab9ff246bf49f9f54810f7c58f278f6009e
xliu <xialiu@quickenloans.com>
2021-12-19 22:53:14 -0500

    148 and 56

--- Sorting/148-Sort List.py
+++ Sorting/148-Sort List.py
@@ -13,3 +13,61 @@ The number of nodes in the list is in the range [0, 5 * 104].
 #### Can you sort the linked list in O(n logn) time: Merge sort and quick sort
 # long(n) memory: merge sort
 # O(1) memory (i.e. constant space)-------------> Quick sort
+
+##### Merge sort:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # sorting with O(nlogn) running time: merge sort and quick sort
+
+        # merge sort  O(nlogn) running time with O(n) momery
+
+        # get the middle:
+        # base case:
+        if not head or not head.next:
+            return head
+        # split the list into two halfs:
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        # divide:
+        left = self.sortList(left)
+        right = self.sortList(right)
+        # conquar
+        return self.merge(left, right)
+
+    # now we can writeh the functions getMid and merge
+    #
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    # merge
+    def merge(self, left, right):
+        dummy = tail = ListNode()
+
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+
+        return dummy.next
---
+++ Sorting/56-Merge Intervals.py
@@ -0,0 +1,39 @@
+"""
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Constraints:
+
+1 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 104
+"""
+# question you might confirm:
+# is it sorted array?
+# stat always <= end?
+# start == previous end
+
+# definitely we need to sort the intervals:
+# after sorting the list, there are 3 different possibilites:
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda i: i[0])
+        output = [intervals[0]]
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+        return output
+
