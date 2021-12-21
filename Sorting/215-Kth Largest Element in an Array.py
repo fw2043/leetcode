060634295dbf1cb3df1db5023ebb4c1ef5a15757
xliu <xialiu@quickenloans.com>
2021-12-21 10:25:44 -0500

    215

--- Sorting/215-Kth Largest Element in an Array.py
+++ Sorting/215-Kth Largest Element in an Array.py
@@ -21,6 +21,43 @@ Constraints:
 # you might be able to use heap
 
 # the better one is: Quick Select
+# the average running time is O(N)
+# the worst case is O(n^2)---> pick the largest one as pivot
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        # Quick Select:
+        # Time complexity:
+        # Average is: O(n)
+        # worst case: O(n^2)
+        # space complexity:
+        # nums = [3,2,1,5,6,4], k = 2
+        # the index of the kth largest elements we are looking for:
+        k = len(nums) - k
+
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+                # here we get  [3,2,1,5,6,4], p =5
+                # at p, we know all elements left of p are smaller than pivot, so swap pivot and p,
+                # we put pivot in the right position
+            nums[p], nums[r] = pivot, nums[p]
+            # now the list is: [3,2,1,4,6,6]
+            # on the left side of p, the left side does not have to be in order, so have to do recursive call
+            if p > k:
+                return quickSelect(l, p - 1)
+            # in the right side, recursive call
+            elif p < k:
+                return quickSelect(p + 1, r)
+            # pivot is the number we are looking for
+            else:
+                return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
+
 
 
 
